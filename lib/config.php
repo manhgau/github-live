ca047063d70153430d1c102c4957fa12644501b5
manhgau <96876095+manhgau@users.noreply.github.com>
2022-03-30 21:54:03 +0700

    Sửa cấu hình url

--- lib/config.php
+++ lib/config.php
@@ -3,7 +3,7 @@
 define("BASE_DIR", dirname(dirname(__file__)));
 define("UPLOAD_DIR", BASE_DIR."/public/upload");
 //
-define("DOMAIN", "http://localhost:8080/live");
+define("DOMAIN", "http://localhost:8080/github-live");
 define("DOMAIN_ADMIN", DOMAIN."/admin");
 
 //cấu hính database
